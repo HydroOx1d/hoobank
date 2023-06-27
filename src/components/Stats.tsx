e40e8f0305fdd7ad9527d5f23e559767cf84465e
HydroOx1d <n-esenov44@mail.ru>
2023-06-27 14:44:44 +0600

    stats

--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -1,7 +1,23 @@
+import {stats} from '../constants'
+import styles from '../styles'
+
 
 const Stats = () => {
   return (
-    <div>Stats</div>
+    <section className={`${styles.flexCenter} flex-row flex-wrap sm:mb-20 mb-6`}>
+      {stats.map((stat) => {
+        return (
+          <div key={stat.id} className={`flex flex-1 justify-start items-center flex-row m-3`}>
+            <h4 className="font-poppins font-semibold xs:text-[40px] text-[30px] xs:leading-[53px] leading-[43px] text-white mr-6">
+              {stat.value}
+            </h4>
+            <p className="font-poppins font-normal xs:text-[20px] text-[16px] xs:leading-[24px] leading-[20px] text-white uppercase text-gradient">
+              {stat.title}
+            </p>
+          </div>
+        );
+      })}
+    </section>
   )
 }
 
