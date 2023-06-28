a17b346318697ff426f190be6f1cc74e3cf912cf
HydroOx1d <n-esenov44@mail.ru>
2023-06-28 13:54:21 +0600

    card deal

--- src/components/CardDeal.tsx
+++ src/components/CardDeal.tsx
@@ -1,8 +1,30 @@
-
+import styles, {layout} from "../styles"
+import cardDeal from '../assets/card.png'
+import { Button } from "./index";
 const CardDeal = () => {
   return (
-    <div>CardDeal</div>
-  )
+    <section className={`${layout.section}`}>
+      <div className={`${layout.sectionInfo}`}>
+        <h2 className={`${styles.heading2} mb-[24px]`}>
+          Find a better card deal <br className="ss:block hidden" /> in few easy
+          steps.
+        </h2>
+        <p className={`${styles.paragraph} mb-[48px] max-w-[470px]`}>
+          Arcu tortor, purus in mattis at sed integer faucibus. Aliquet quis
+          aliquet eget mauris tortor.ç Aliquet ultrices ac, ametau.
+        </p>
+
+        <Button>Get started</Button>
+      </div>
+      <div className={`${layout.sectionImg}`}>
+        <img
+          src={cardDeal}
+          alt="card deal"
+          className="w-[100%] h-[100%] object-contain"
+        />
+      </div>
+    </section>
+  );
 }
 
 export default CardDeal
