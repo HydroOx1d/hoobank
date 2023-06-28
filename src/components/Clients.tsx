1d9f08164b53d5683adba59f8feee097cf4824bd
HydroOx1d <n-esenov44@mail.ru>
2023-06-28 18:24:29 +0600

    clients

--- src/components/Clients.tsx
+++ src/components/Clients.tsx
@@ -1,8 +1,25 @@
+import { clients } from "../constants";
 
 const Clients = () => {
   return (
-    <div>Clients</div>
-  )
-}
+    <section
+      id="clients"
+      className={`py-[40px] w-full flex items-center flex-wrap`}
+    >
+      {clients.map((client) => {
+        return (
+          <div className="flex-1 basis-[100%] md:mb-0 mb-3 md:basis-[25%] ss:basis-[50%]">
+            <img
+              src={client.logo}
+              alt={client.id}
+              key={client.id}
+              className="w-[200px] h-[60px] object-contain mx-auto px-[10px]"
+            />
+          </div>
+        );
+      })}
+    </section>
+  );
+};
 
-export default Clients
+export default Clients;
